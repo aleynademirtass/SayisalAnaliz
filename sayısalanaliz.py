f406b4619a3724259ecff6bacd2ce53758d99c8e
aleynademirtass <129094406+aleynademirtass@users.noreply.github.com>
2023-11-05 21:51:24 +0300

    Delete sayısalanaliz.py

--- sayısalanaliz.py
+++
@@ -1,9 +0,0 @@
-import math
-a=math.pi/5
-x=math.cos(a)
-z=math.cos(0)-(math.sin(0)*(a-0))
-kesmehata1=x-z
-print('bir terimli taylor seri aciliminda olusan kesme hatasi:',kesmehata1)
-e=math.cos(0)-(math.sin(0)*(a-0))-(math.cos(0)*a*a/2)
-kesmehata2=x-e
-print('iki terimli taylor seri aciliminda olusan kesme hatasi:',kesmehata2)
